9da50d4605e855e37097266ee386c3b417792c30
Marco Behler <marco@marcobehler.com>
2022-08-05 15:05:43 +0200

    Hibernate added

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "BIRTH_DATE")
+    private LocalDate birthDate;
+
+    public User() {
+    }
+
+    public User(String name, LocalDate birthDate) {
+	   this.name = name;
+	   this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+	   return id;
+    }
+
+    public void setId(Long id) {
+	   this.id = id;
+    }
+
+    public String getName() {
+	   return name;
+    }
+
+    public void setName(String name) {
+	   this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+	   return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+	   this.birthDate = birthDate;
+    }
+
+}
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,7 +1,17 @@
 package org.example;
 
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,9 +20,53 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class AppTest {
 
+    private SessionFactory sessionFactory;
+
+    @BeforeEach
+    protected void setUp() throws Exception {
+	   // A SessionFactory is set up once for an application!
+	   final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+			 .configure() // configures settings from hibernate.cfg.xml
+			 .build();
+	   try {
+		  sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+	   }
+	   catch (Exception e) {
+		  // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+		  // so destroy it manually.
+		  StandardServiceRegistryBuilder.destroy( registry );
+	   }
+    }
+
+    @AfterEach
+    protected void tearDown() throws Exception {
+	   if ( sessionFactory != null ) {
+		  sessionFactory.close();
+	   }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testBasicUsage() {
+	   // create a couple of events...
+	   Session session = sessionFactory.openSession();
+	   session.beginTransaction();
+	   session.persist(new User("Marco's Friend", LocalDate.now()));
+	   session.getTransaction().commit();
+	   session.close();
+
+	   session = sessionFactory.openSession();
+	   session.beginTransaction();
+	   List<User> result = session.createQuery( "select u from User u" , User.class).list();
+	   for ( User user : result) {
+		  System.out.println( "User (" + user.getName() + ") : " + user.getBirthDate() );
+	   }
+	   session.getTransaction().commit();
+	   session.close();
+    }
+
     @Test
     public void marco_is_in_the_house() {
-
 	   assertThat(1).isGreaterThanOrEqualTo(0);
     }
 }
