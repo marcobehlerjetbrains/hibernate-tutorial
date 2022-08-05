90ee47d40313f545f0e552556afecc517d53b0bb
Marco Behler <marco@marcobehler.com>
2022-08-05 14:01:59 +0200

    Initial project skeleton

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+	* Rigorous Test :-)
+	*/
+    @Test
+    public void bla() {
+	   assertThat(1).isGreaterThanOrEqualTo(0);
+    }
+}
